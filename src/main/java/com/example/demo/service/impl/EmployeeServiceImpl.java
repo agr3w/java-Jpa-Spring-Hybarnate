f1f9d001013e53f58a1481c96cfde9d1c2508309
Weslley Luiz Kampa <91283681+agr3w@users.noreply.github.com>
2025-05-14 15:17:08 -0300

    Adicionando comandos no service

--- src/main/java/com/example/demo/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.demo.dto.EmployeeDto;
 import com.example.demo.entity.Employee;
+import com.example.demo.exception.ResourceNotFound;
 import com.example.demo.mapper.EmployeeMapper;
 import com.example.demo.repository.EmployeeRepository;
 import com.example.demo.service.EmployeeService;
@@ -21,6 +22,13 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
         Employee savedEmployee = employeeRepository.save(employee);
         return EmployeeMapper.mapToEmployeeDto(savedEmployee); 
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        employeeRepository.findById(employeeId)
+            .orElseThrow(() -> new ResourceNotFound("Employee is not exist with given id: " + employeeId));
+        return null;
     }
     
 }
